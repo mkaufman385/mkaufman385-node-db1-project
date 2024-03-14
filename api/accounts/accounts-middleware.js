a8ee9c41e59694dfe69723c77f9712a064c2113b
mkaufman385 <mkaufman385@gmail.com>
2024-03-14 12:39:06 -0400

    wip

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,8 +1,15 @@
 const Account = require("./accounts-model");
 
 exports.checkAccountPayload = (req, res, next) => {
-  console.log("checkAccountPayload middleware");
-  next();
+  const error = { status: 400 };
+  const { name, budget } = req.body;
+  if (name === undefined || budget === undefined) {
+    error.message = "name and budget are required";
+    next(error);
+  } else if (typeof name !== "string") {
+    error.message = "name of account must be a string";
+    next(error);
+  }
 };
 
 exports.checkAccountNameUnique = (req, res, next) => {
